9f7bbb38c9e0d9645729101b7be6e297f7bd64ba
Kotxic <nesterov.d.c@mail.ru>
2024-03-22 14:02:24 +0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {useEffect, useMemo, useState} from "react";
+import axios from "axios";
+import './style/style.css'
+import MySelect from "./UI/MySelect/MySelect";
+import Search from "./Components/Search/Search";
+import Table from "./Components/Table/Table";
+import MyButton from "./UI/MyButton/MyButton";
+import API from "./API/API";
 function App() {
+  const [users, setUsers]=useState([])
+  const [searchSelector, setSearchSelector]=useState('city')
+  const [searchQuery, setSearchQuery]=useState('')
+  const [limit, setLimit]=useState(5)
+  const [page, setPage]=useState(1)
+
+    useEffect(()=>{
+        getPost()
+    }, [])
+    async function getPost(){
+        const responce = await API.getUsers()
+        setUsers(responce.data)
+    }
+
+  const sortedUsers = useMemo(()=>{
+      setPage(1)
+    if(searchQuery===''){
+        return users
+    }
+    if(searchSelector==='city'){
+        return users.filter(user =>user.address.city.toLowerCase().includes(searchQuery.toLowerCase()))
+    }
+    if(searchSelector==='name'){
+      return users.filter(user =>user.name.toLowerCase().includes(searchQuery.toLowerCase()))
+    }
+    if(searchSelector==='email'){
+      return users.filter(user =>user.email.toLowerCase().includes(searchQuery.toLowerCase()))
+    }
+
+  },[searchQuery, users])
+
+
+  function setCity(value){
+      setSearchSelector('city')
+      setSearchQuery(value)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Search searchSelector={searchSelector} setSearchSelector={setSearchSelector} setSearchQuery={setSearchQuery} searchQuery={searchQuery}/>
+      <MySelect
+          value={searchQuery}
+          onChange={setCity}
+          defaultValue='Выбор города'
+          options={[
+            {name:'Gwenborough', value:'Gwenborough'},
+            {name:'Wisokyburgh', value:'Wisokyburgh'},
+            {name:'McKenziehaven', value:'McKenziehaven'},
+            {name:'South Elvis', value:'South Elvis'},
+            {name:'Roscoeview', value:'Roscoeview'},
+            {name:'South Christy', value:'South Christy'},
+            {name:'Howemouth', value:'Howemouth'},
+            {name:'Aliyaview', value:'Aliyaview'},
+            {name:'Bartholomebury', value:'Bartholomebury'},
+            {name:'Lebsackbury', value:'Lebsackbury'}
+          ]}
+      />
+
+      <Table page={page} sortedUsers={sortedUsers}/>
+      <div className={sortedUsers.length>limit ?'buttons' : 'buttons__disable' }>
+          <MyButton disabled={page===1}  onClick={()=>setPage(page-1)}>Предыдущая страница</MyButton>
+          <MyButton disabled={sortedUsers.length<=(limit*page)} onClick={()=>setPage(page+1)}>Следующая страница</MyButton>
+      </div>
+
     </div>
   );
 }
